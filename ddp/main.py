2fd986ec7bef01fbd04b3f7c3a7336de929d0c77
Nicholas Eisenberg <EISENBNT@nv.doe.gov>
2024-03-25 15:02:44 -0700

    DDP runs

--- ddp/main.py
+++ ddp/main.py
@@ -1,6 +1,19 @@
+import os
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
+import torch.multiprocessing as mp
+from torch.utils.data.distributed import DistributedSampler
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.distributed import init_process_group, destroy_process_group
+
+
+
+def ddp_setup(rank, world_size):
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "12345"
+    init_process_group(backend="nccl", rank=rank, world_size=world_size)
+
 
 class Trainer:
     def __init__(self,
@@ -11,11 +24,11 @@ class Trainer:
                  gpu_id: int,
                  save_every: int):
         self.gpu_id = gpu_id
-        self.model = model
         self.train_loader = train_loader
         self.optimizer = optimizer
         self.loss_fn = loss_fn
         self.save_every = save_every
+        self.model = DDP(model, device_ids=[self.gpu_id])
 
     def _batch_pass(self, inputs, targets):
         self.optimizer.zero_grad()
@@ -31,14 +44,14 @@ class Trainer:
             self._batch_pass(inputs, targets)
 
     def _save_checkpoint(self, epoch):
-        ckp = self.model.state_dict()
+        ckp = self.model.module.state_dict()
         torch.save(ckp, "checkpoint.pt")
         print(f"EPOCH {epoch} CHECKPOINT SAVED")
 
     def train(self, max_epochs: int):
         for epoch in range(1, max_epochs + 1):
             self._run_epoch(epoch)
-            if epoch % self.save_every == 0:
+            if self.gpu_id == 0 and epoch % self.save_every == 0:
                 self._save_checkpoint(epoch)
 
 
@@ -75,27 +88,41 @@ def load_train_objects():
     optimizer = torch.optim.Adam(model.parameters(), lr=.001)
     return train_dataset, model, loss_fn, optimizer
 
+
 def format_dataloader(dataset: Dataset, batch_size: int):
-    return DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    return DataLoader(
+        dataset, 
+        batch_size=batch_size, 
+        shuffle=False,
+        sampler=DistributedSampler(dataset)
+    )
 
 
-def main(device, total_epochs, save_every):
+def main(rank, world_size, total_epochs, save_every):
+    ddp_setup(rank, world_size)
     train_dataset, model, loss_fn, optimizer = load_train_objects()
-    model = model.to(device)
+    model = model.to(rank)
     train_loader = format_dataloader(train_dataset, 32)
     trainer = Trainer(
         model=model, 
         train_loader=train_loader, 
         loss_fn=loss_fn, 
         optimizer=optimizer, 
-        gpu_id=device, 
+        gpu_id=rank, 
         save_every=save_every
     )
     trainer.train(total_epochs)
+    destroy_process_group()
 
 
 if __name__ == "__main__":
     total_epochs = 5
     save_every = 1
-    device = 0
-    main(device, total_epochs, save_every)
+    world_size = torch.cuda.device_count()
+    mp.spawn(main, args=(world_size, total_epochs, save_every), nprocs=world_size)
+
+
+
+
+
+
