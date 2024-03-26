00ed75df5988aaeaadfe45f5d3b6e668291f29f6
nickeisenberg <nickeisenberg@gmail.com>
2024-03-25 20:44:38 -0700

    multi node multi gpu runs with slurm on LLNL

--- ddp/multi_node/run/slurm/run.sh
+++ ddp/multi_node/run/slurm/run.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
 #SBATCH --job-name=multinode-example
+#SBATCH --output=output_%j.log
+#SBATCH --error=error_%j.log
 #SBATCH --nodes=2
-#SBATCH --ntasks=4
+#SBATCH --ntasks=2
 #SBATCH --gpus-per-task=2
-#SBATCH --cpus-per-task=4
 
 nodes=( $( scontrol show hostnames $SLURM_JOB_NODELIST ) )
 nodes_array=($nodes)
 head_node=${nodes_array[0]}
 head_node_ip=$(srun --nodes=1 --ntasks=1 -w "$head_node" hostname --ip-address)
 
+echo SLURM_JOB_LIST $SLURM_JOB_NODELIST
+echo NODES $nodes
+echo HEAD NODE $head_node
 echo Node IP: $head_node_ip
 export LOGLEVEL=INFO
 
 srun torchrun \
---nnodes 4 \
---nproc_per_node 1 \
---rdzv_id $RANDOM \
---rdzv_backend c10d \
---rdzv_endpoint $head_node_ip:29500 \
-../../ddp_multi_node_torchrun.py
+    --nnodes 2 \
+    --nproc_per_node 2 \
+    --rdzv_id $RANDOM \
+    --rdzv_backend c10d \
+    --rdzv_endpoint $head_node_ip:29500 \
+    ../../ddp_multi_node_torchrun.py
